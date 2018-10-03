2a0de104554e07158a1e3b46f7f50f79b3cecd67
Chisimdi Damian Ezeanieto <chisimdi@Chisimdis-MacBook-Pro.local>
2018-10-03 23:45:41 +0100

    done with creating profile and updating profile.

--- models/Profile.model.js
+++ models/Profile.model.js
@@ -2,45 +2,45 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ProfileSchema = new Schema({
-    user: { type: Schema.type.ObjectId, ref: 'users' },
-    handle: { type: String, required: true, max: 40 },
-    company: { type: String, required: false },
-    website: { type: String, required: false },
-    location: { type: String, required: false },
-    status: { type: String, required: true },
-    skills: { type: [ String ], required: true },
-    bio: { type: String, required: false },
-    githubUsername: { type: String, required: false },
+    user: { type: Schema.Types.ObjectId, ref: 'users' },
+    handle: { type: Schema.Types.String, required: true, max: 40 },
+    company: { type: Schema.Types.String, required: false },
+    website: { type: Schema.Types.String, required: false },
+    location: { type: Schema.Types.String, required: false },
+    status: { type: Schema.Types.String, required: true },
+    skills: { type: [ Schema.Types.String ], required: true },
+    bio: { type: Schema.Types.String, required: false },
+    githubUsername: { type: Schema.Types.String, required: false },
     experience: [
         { 
-            title: { type: String, required: true },
-            company: { type: String, required: true },
-            location: { type: String, required: true },
-            from: { type: Date, required: true },
-            to: { type: Date, required: false },
+            title: { type: Schema.Types.String, required: true },
+            company: { type: Schema.Types.String, required: true },
+            location: { type: Schema.Types.String, required: true },
+            from: { type: Schema.Types.Date, required: true },
+            to: { type: Schema.Types.Date, required: false },
             current: { type: Boolean, required: false, default: false },
-            description: { type: String, required: false }
+            description: { type: Schema.Types.String, required: false }
         }
     ],
     education: [
         { 
-            school: { type: String, required: true },
-            degree: { type: String, required: true },
-            fieldOfStudy: { type: String, required: true },
-            from: { type: Date, required: true },
-            to: { type: Date, required: false },
-            current: { type: Boolean, required: false, default: false },
-            description: { type: String, required: false }
+            school: { type: Schema.Types.String, required: true },
+            degree: { type: Schema.Types.String, required: true },
+            fieldOfStudy: { type: Schema.Types.String, required: true },
+            from: { type: Schema.Types.Date, required: true },
+            to: { type: Schema.Types.Date, required: false },
+            current: { type: Schema.Types.Boolean, required: false, default: false },
+            description: { type: Schema.Types.String, required: false }
         }
     ],
     social: {
-        youtube: { type: String, required: false },
-        twitter: { type: String, required: false },
-        facebook: { type: String, required: false },
-        linkedin: { type: String, required: false },
-        instagram: { type: String, required: false }
+        youtube: { type: Schema.Types.String, required: false },
+        twitter: { type: Schema.Types.String, required: false },
+        facebook: { type: Schema.Types.String, required: false },
+        linkedin: { type: Schema.Types.String, required: false },
+        instagram: { type: Schema.Types.String, required: false }
     },
-    date: { type: Date, default: Date.now() }
+    date: { type: Schema.Types.Date, default: Date.now() }
 });
 
 module.exports = Profile = mongoose.model('Profiles', ProfileSchema);
--- services/profiles.service.js
+++ services/profiles.service.js
@@ -3,6 +3,10 @@
     @Date: 30/10/2018
 */
 const express = require('express');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const Profile = require('../models/Profile.model');
+const User = require('../models/User.model');
 const router = express.Router();
 
 router.get('/test', (req, res) => {
@@ -12,5 +16,87 @@ router.get('/test', (req, res) => {
         data: []
     });
 });
+
+/* 
+    @Route: GET /api/profile
+    @Desc: API to get current user's profile
+    @Access: Private
+    @Params: none
+*/
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const userId = req.user.id;
+    const errors = {};
+    Profile.findOne({ user: userId }).then(profile => {
+        errors.profile = 'There is no profile for this user';
+        if (!profile) return res.status(404).json({ status: 404, msg: 'There is no profile for this user', errors });
+
+        res.json({ status: 200, msg: 'User profile', data: [profile]});
+    }).catch(err => {
+        errors = err;
+        res.status(404).json({ status: 404, msg: 'Error!', errors});
+    });
+});
+
+
+/* 
+    @Route: POST /api/profile
+    @Desc: API to create/edit user's profile
+    @Access: Private
+    @Params: none
+*/
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    // Get all the fields supplied by the user
+    const profileFields = {};
+    const errors = {};
+    const userId = req.user.id;
+    profileFields.user = userId;
+
+    if (req.body.handle) profileFields.handle = req.body.handle; 
+    if (req.body.company) profileFields.company = req.body.company; 
+    if (req.body.website) profileFields.website = req.body.website; 
+    if (req.body.location) profileFields.location = req.body.location; 
+    if (req.body.bio) profileFields.bio = req.body.bio; 
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubUsername) profileFields.githubUsername = req.body.githubUsername;
+    // Skills is coming in as a string of CSV. Hence split into an array
+    if(typeof req.body.skills !== 'undefined') {
+        profileFields.skills = req.body.skills.split(',');
+    }
+    // Social
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({ user: userId }).then(profile => {
+        if (profile) {
+            // This is an update
+            Profile.findOneAndUpdate({ user: userId }, { $set: profileFields }, { new: true }).then(update => {
+                res.json({ status: 200, msg: 'User profile', data: [update]});
+            });
+        } else {
+            // This is a create
+            // Check if handle exists
+            Profile.findOne({ handle: profileFields.handle }).then(foundHandle => {
+                errors.handle = 'This handle already exists';
+                if (foundHandle) res.status(400).json({ status: 400, msg: 'TThis handle already exists', errors });
+
+                // Save Profile
+                new Profile(profileFields).then(create => {
+                    res.json({ status: 200, msg: 'User profile', data: [create]});
+                }).catch(err => {
+                    errors = err;
+                    res.status(404).json({ status: 404, msg: 'Error!', errors})
+                });
+            });
+        }
+    }).catch(err => {
+        errors = err;
+        res.status(404).json({ status: 404, msg: 'Error!', errors})
+    });    
+});
+
 
 module.exports = router;
