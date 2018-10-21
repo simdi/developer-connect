591d851349723ccaa9208d3d8b20caf42d480698
Chisimdi Damian Ezeanieto <chisimdi@Chisimdis-MacBook-Pro.local>
2018-10-22 00:00:59 +0100

    Done with the landing page.

---
+++ client/src/components/layouts/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="bg-dark text-center text-white mt-5 p-4">
+      Copyright &copy; { new Date().getFullYear() } Developers Connect.
+    </footer>
+  )
+}
