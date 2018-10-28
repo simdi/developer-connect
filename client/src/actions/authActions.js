df7c43ab178897b80f8f32d5910a3b9930649f41
Chisimdi Damian Ezeanieto <chisimdi@Chisimdis-MacBook-Pro.local>
2018-10-29 00:12:29 +0100

    Done with registration.

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 import { GET_ERRORS } from './types';
 
 // Register User
-export const registerUser = (userData) => dispatch => {
+export const registerUser = (userData, history) => dispatch => {
     axios.post('/api/users/register', userData).then(res => {
       console.log('Res', res);
+        history.push('/login');
     }).catch(err => {
       console.log('Error', err.response.data.errors);
       dispatch({
