f232df52f78d78aaa3f665411331baeeb131f89a
Chisimdi Damian Ezeanieto <chisimdi@Chisimdi.local>
2018-11-02 01:13:57 +0100

    Started working on dashboard.

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_ERRORS, GET_PROFILE, GET_PROFILES, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from './types';
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from './types';
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
